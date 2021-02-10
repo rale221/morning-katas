6fadaab767e25f296df494bfeeda8edbf2600b90
Rastko <rastkodjurica@yahoo.com>
2021-02-10 00:48:20 -0500

    GrassHopper

---
+++ 8kyu/grassHopper-basicFunctionFixer.js
@@ -0,0 +1,16 @@
+// Fix the function
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+// https://www.codewars.com/kata/56200d610758762fb0000002/train/javascript
+
+
+// Option: 1
+function addFive(num) {
+    return (num + 5)
+}
+
+
+// Option: 2
+const addFive = num => num + 5
