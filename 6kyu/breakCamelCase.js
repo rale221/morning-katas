436f411efe8e43204b0cd1da38cfaf6fb835aeb0
Rastko <rastkodjurica@yahoo.com>
2021-02-10 00:38:16 -0500

    2/9

---
+++ 6kyu/breakCamelCase.js
@@ -0,0 +1,39 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// solution("camelCasing")  ==  "camel Casing"
+
+//https://www.codewars.com/kata/5208f99aee097e6552000148
+
+//Solution 1 with for loop
+function solution(string) {
+    let newString = "";
+    for (let i of string) {
+        if (i === i.toUpperCase()) {
+            newString += ` ${i}`;
+        } else {
+            newString += i;
+        }
+    }
+    return newString;
+}
+
+//Solution 2 with .map()
+function solution(string) {
+    return string
+        .split("")
+        .map((letter) => {
+            if (letter === letter.toUpperCase()) {
+                return ` ${letter}`;
+            } else {
+                return letter;
+            }
+        })
+        .join("");
+}
+
+//Solution 3
+function solution(string) {
+    //use capturing group and numbered backreference in regex
+    return string.replace(/([A-Z])/g, " $1");
+}
