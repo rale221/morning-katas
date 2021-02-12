7efa9cc035e5c38fb099e4f9fe1c8c01c03b590f
Rastko <rastkodjurica@yahoo.com>
2021-02-11 23:10:32 -0500

    stringrepeat

---
+++ 8kyu/stringrepeat.js
@@ -0,0 +1,21 @@
+// Write a function called repeat_str which repeats the given string src exactly count times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+// https://www.codewars.com/kata/56200d610758762fb0000002/train/javascript
+
+
+// Option: 1
+function repeatStr(n, s) {
+    let str = '';
+    for (let i = 0; i < n; i++) {
+        str += s
+    }
+    return str;
+    return s.repeat(n)
+}
+
+
+// Option: 2
+const repeatStr = (n, s) => s.repeat(n)
