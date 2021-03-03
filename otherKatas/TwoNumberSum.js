50ed420081b87efff046a5c30d733bd575bb6a24
Rastko <rastkodjurica@yahoo.com>
2021-03-03 09:34:09 -0500

    4loopTwoNumbers

--- otherKatas/TwoNumberSum.js
+++ otherKatas/TwoNumberSum.js
@@ -5,6 +5,14 @@
 
 function twoNumberSum(array, targetSum) {
     //Your code goes here
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === targetSum) {
+                return [array[i], array[j]]
+            }
+        }
+    }
+    return []
 
 }
 
