487af78ebb746eff2eae9a95e4d2d6fd0278ae98
Rastko <rastkodjurica@yahoo.com>
2021-03-09 11:09:40 -0500

    SolutionOne

---
+++ 6kyu/ObtaingTheString.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/5bab83fffe6c0c52b8000172/train/javascript
+
+//You are given two strings s and t. Both strings have length n and consist of lowercase Latin letters.
+
+//You can successively perform the following move any number of times (possibly, zero):
+
+//swap any two adjacent (neighboring) characters of s (i.e. for any i={0,1,2,…,n−2} you can swap si and si+1).
+
+//*Input:**
+//Strings s and t, which consisting of n(1 ≤ n ≤ 1000) lowercase Latin letters.
+
+//Output:
+//The minimum number of moves to transform s to t. If it is impossible to obtain the string t using moves, return -1.
+
+function obtain(s, t) {
+	//   let counter = 0
+	//   for(let i = 0; i<s.lemgth; i++){
+	//     let index = t.indexOf(s[i])
+	//     console.log(index)
+	//     if(index=== -1) return -1
+	//     t = t.slice(0, 1) + t.slice(index+1)
+	//     console.log(t)
+	//     counter += index
+	//   }
+	//   console.log(counter)
+
+	if (imput.split('').sort().join('') != target.split('').sort().join('')) {
+		ret;
+	}
+}
