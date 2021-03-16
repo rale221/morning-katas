2c1b03aedfcc673625e48fd82ed1909f355c4553
Rastko <rastkodjurica@yahoo.com>
2021-03-15 22:43:28 -0400

    Oposite Number Solution One

---
+++ 8kyu/OpositeNumber.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+// Very simple, given a number, find its opposite.
+
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+function oposite(number) {
+	return -number;
+}
