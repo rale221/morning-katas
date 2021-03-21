6bdb7496568cde66311980f197d606a04f65997a
Rastko <rastkodjurica@yahoo.com>
2021-03-21 01:20:43 -0400

    Kristina's Lazy Ass Speed

---
+++ 8kyu/KristinasLazySpeed.js
@@ -0,0 +1,17 @@
+// Calculate how much Kristina is lazy on weekends. And how fast she goes from one side of the bed to another side of the bad.
+
+tvshow = 30
+function lazyass(tvshow){
+
+     let fromAtoB = 1.5
+     let lazyAssSpeed = fromAtoB / tvshow
+     if ( tvshow < 30){
+          return lazyAssSpeed *100
+     } if (tvshow < 45) {
+          return lazyAssSpeed *100
+     }else {
+          return lazyAssSpeed * 100
+     }
+     
+}
+
