bd7d014c2ec70e174273540dfe0c519762f0ad54
Rastko <rastkodjurica@yahoo.com>
2021-03-21 00:07:28 -0400

    Solution One Done

--- 8kyu/TransportationOnvacation.js
+++ 8kyu/TransportationOnvacation.js
@@ -10,31 +10,12 @@
 
 function rentalCarCost(d) {
      // Your solution here
-    let price = 0
-    if (d < 3){
-      return d*40 
-    } if else (d < 5){
-      return d*40 - 20
-    } else {
-      return d*40 - 50
-    }
+     let price = d*40
+     if (d < 3){
+       return price
+     } if (d < 7){
+       return price - 20
+     } else {
+       return price - 50
+     }
    }
-
-   Test.describe("Regular Tests:", function() {
-     Test.it("Should work for under 3 days:", function() {
-       Test.assertEquals(rentalCarCost(1), 40);
-       Test.assertEquals(rentalCarCost(2), 80);
-     });
-     Test.it("Should work for under 7 days:", function() {
-       Test.assertEquals(rentalCarCost(3), 100);
-       Test.assertEquals(rentalCarCost(4), 140);
-       Test.assertEquals(rentalCarCost(5), 180);
-       Test.assertEquals(rentalCarCost(6), 220);
-     });
-     Test.it("Should work for 7 or more days:", function() {
-       Test.assertEquals(rentalCarCost(7), 230);
-       Test.assertEquals(rentalCarCost(8), 270);
-       Test.assertEquals(rentalCarCost(9), 310);
-       Test.assertEquals(rentalCarCost(10), 350);    
-     });
-   });
