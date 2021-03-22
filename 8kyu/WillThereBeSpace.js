8bfa65fbfebb609332531eef1a10992bdd17a9dd
Rastko <rastkodjurica@yahoo.com>
2021-03-22 17:59:44 -0400

    Enought Space in The Buss

---
+++ 8kyu/WillThereBeSpace.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5875b200d520904a04000003/train/javascript
+
+//Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, 
+//he sometimes has to face the problem of not enough space left on the bus! 
+//He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
