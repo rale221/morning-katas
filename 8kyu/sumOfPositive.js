b1be504bd5cbe92376e553f60aa85dff489f947d
Rastko <rastkodjurica@yahoo.com>
2021-03-24 19:45:57 -0400

    Sum Of Positive

--- 8kyu/sumOfPositive.js
+++ 8kyu/sumOfPositive.js
@@ -7,27 +7,36 @@
 // https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
 
 //Solution
+
+arr = [1,-2, 3, 4, 5]
 function positiveSum(arr) {
 
     // filter positive numbers
     // return total
 
-    let total = 0
+    // let total = 0
 
-    for (let i = 0; i < arr.length; i++) {
-        total += arr[i] * (arr[i] > 0)
+    // for (let i = 0; i < arr.length; i++) {
+    //     total += arr[i] * (arr[i] > 0)
 
         //   if(arr[i]> 0){
         //     total += arr[i]
         //   }
 
-        arr[i] > 0 ? total += arr[i] : total
+    //     arr[i] > 0 ? total += arr[i] : total
 
-        total += (arr[i] > 0 ? arr[i] : 0)
-    }
-    return total
+    //     total += (arr[i] > 0 ? arr[i] : 0)
+    // }
+    // return total
 
     // return arr.reduce((total, eachElement) => {
     //   return total + ((eachElement >0)? eachElement : 0)
     // },0 )
+   
+    let total = 0
+    for (i=0; arr < arr.length; i++){
+        total += arr[i] * (arr[i] > 0)
+        
+    }
+
 }
