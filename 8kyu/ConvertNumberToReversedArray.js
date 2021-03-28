0fbf1aaf8aaf809094013952c1de468052abb02f
Rastko <rastkodjurica@yahoo.com>
2021-03-27 23:37:24 -0400

    Convert NUmber To Reverse Array

---
+++ 8kyu/ConvertNumberToReversedArray.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/javascript
+
+// Given a random non-negative number,
+// you have to return the digits of this number within an array in reverse order.
+
+// Example:
+
+// 348597 => [7,9,5,8,4,3]
+
+function digitize(n) {
+     //code here
+   }
