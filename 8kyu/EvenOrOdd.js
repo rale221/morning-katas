95a1548361f97aee84dcb9b31145a20838977e94
Rastko <rastkodjurica@yahoo.com>
2021-03-28 18:42:20 -0400

    Odd Even boolean solution

--- 8kyu/EvenOrOdd.js
+++ 8kyu/EvenOrOdd.js
@@ -3,5 +3,10 @@
 // Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 function even_or_odd(number) {
-     
-}
+     let result = ''
+     if (number % 2 != 0){
+       return 'Odd'
+     } else {
+       return 'Even'
+     }
+     }
