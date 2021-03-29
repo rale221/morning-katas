e5ce4c16cc4a9d335f646c243aa40f0006d8d0c2
Rastko <rastkodjurica@yahoo.com>
2021-03-29 19:06:09 -0400

    Alan Partrigel II Solution

--- 8kyu/AlanPartridgeII.js
+++ 8kyu/AlanPartridgeII.js
@@ -4,3 +4,20 @@
 // According to space.com the temperature of the sun's corona is 2,000,000 degrees C, but we will ignore the science for now.
 // Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
 // X will be either a number or a string. Both are valid.
+
+// x = '23'
+// x = 74
+
+
+function apple(x){
+     if (typeof x === 'string'){
+          x = parseInt(x)
+     }
+     if (x * x > 1000){
+          return 'It\'s hotter than the sun'
+     }
+     else {
+          return 'Help yourself to a honeycomb Yorkie for the glovebox '
+     }
+}
+
