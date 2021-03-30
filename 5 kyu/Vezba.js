ccfc2ed4cc2d8b37c1d0be82ea70d4002e77a7e2
Rastko <rastkodjurica@yahoo.com>
2021-03-30 15:59:26 -0400

    Practice String to Number Map Array list of Numbers

--- 5 kyu/Vezba.js
+++ 5 kyu/Vezba.js
@@ -1,14 +1,67 @@
-const a1 = [1, '2', 3]
-let suma1 = 0;
-const fja = () => {
-  for(let i=0; i< a1.length; i++) {
-    if(typeof a1[i] === 'string') {
-        a1[i] = +a1[i] // a1[i] = parseInt(a1[i])
-        console.log('element je string', a1[i])
-    }
-    suma1 = suma1 + a1[i] // suma += a1[i]
+// const a1 = [1, '2', 3]
+// let suma1 = 0;
+// const fja = () => {
+//   for(let i=0; i< a1.length; i++) {
+//     if(typeof a1[i] === 'string') {
+//         a1[i] = +a1[i] // a1[i] = parseInt(a1[i])
+//         console.log('element je string', a1[i])
+//     }
+//     suma1 = suma1 + a1[i] // suma += a1[i]
+// }
+// return suma1;
+// }
+// const resenj = fja(a1);
+// console.log('resenje je', resenj)
+
+// const a1 = [1, '2', 3, '4'];
+// let suma1 = 0;
+// const fja = () => {
+// 	// element je trenutni element u nizu
+// 	a1.map((element) => {
+// 		if (typeof element === 'string') {
+// 			element = +element; // a1[i] = parseInt(a1[i])
+
+// 			console.log('string element je', element);
+// 		}
+// 		suma1 = suma1 + element; // suma += a1[i]
+// 	});
+
+// 	return suma1;
+// };
+// const resenj = fja(a1);
+// console.log('resenje je', resenj);
+
+// // const a = (callback) => {
+// 	callback();
+// };
+
+// const b = () => {
+// 	console.log('ispis');
+// };
+
+// a(b); // pozivam fju a koja ima parametar  fju b
+
+// niz = [1, 2, 3, 4, 5];
+
+// function print(array) {
+// 	array.map((element) => {
+// 		// console.log('element je', element);
+// 		ispis(element);
+// 	});
+// }
+
+// const ispis = (e) => {
+// 	// console.log('ISPIS', e);
+// 	console.log(`ISPIS ${e}`);
+// };
+
+a = 5;
+b = 20;
+function niz(a, b) {
+	let arr = [];
+	for (let i = a; i < b; i++) {
+		console.log('element je', i);
+		arr.push(i);
+	}
+	return arr;
 }
-return suma1;
-}
-const resenj = fja(a1);
-console.log('resenje je', resenj)
