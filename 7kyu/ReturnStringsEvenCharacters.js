f692c5d1cd06f01e656adde5ac959cf1c354aa27
Rastko <rastkodjurica@yahoo.com>
2021-04-02 18:45:05 -0400

    Not Done

--- 7kyu/ReturnStringsEvenCharacters.js
+++ 7kyu/ReturnStringsEvenCharacters.js
@@ -1,7 +1,6 @@
 // https://www.codewars.com/kata/566044325f8fddc1c000002c/train/javascript
 
-
-// Write a function that returns a sequence (index begins with 1) of all the even characters from a string. 
+// Write a function that returns a sequence (index begins with 1) of all the even characters from a string.
 // If the string is smaller than two characters or longer than 100 characters,
 // the function should return "invalid string".
 
@@ -10,3 +9,19 @@
 // "abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
 // "a"             --> "invalid string"
 
+string = ['abcdefghijklm'];
+
+function evenChars(string) {
+	// if (string.length < 2 || string.length > 100) {
+	// 	return 'invalid string';
+	// } else {
+	let s = string.split(' ');
+	return s;
+	// return string
+	// 	.split('')
+	// 	.filter(function (item, pos, self) {
+	// 		return self.indexOf(item) == pos;
+	// 	})
+	// 	.join('');
+	// // }
+}
