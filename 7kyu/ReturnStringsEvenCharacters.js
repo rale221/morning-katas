6384de56ceb2e9aaaddb70e7fc5d19e57c9932b4
Rastko <rastkodjurica@yahoo.com>
2021-04-02 18:11:40 -0400

    String Even Characters

---
+++ 7kyu/ReturnStringsEvenCharacters.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/566044325f8fddc1c000002c/train/javascript
+
+
+// Write a function that returns a sequence (index begins with 1) of all the even characters from a string. 
+// If the string is smaller than two characters or longer than 100 characters,
+// the function should return "invalid string".
+
+// For example:
+
+// "abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+// "a"             --> "invalid string"
+
