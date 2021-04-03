5a7f88d9f30943b5edf08f51bd551170c76f4aeb
Rastko <rastkodjurica@yahoo.com>
2021-04-02 23:50:23 -0400

    Solution 3

--- 7kyu/ReturnStringsEvenCharacters.js
+++ 7kyu/ReturnStringsEvenCharacters.js
@@ -36,3 +36,16 @@ function evenChars(s) {
 	}
 	return output;
 }
+
+// Solution 4
+
+function evenChars(string) {
+	if ((string.length < 2) | (string.length > 100)) {
+		return 'invalid string';
+	}
+	let result = [];
+	for (i = 1; i < string.length; i += 2) {
+		result.push(string[i]);
+	}
+	return result;
+}
