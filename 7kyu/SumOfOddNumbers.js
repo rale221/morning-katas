5aa36d91522cade08307f55e62a1c0c55b0f4b87
Rastko <rastkodjurica@yahoo.com>
2021-04-06 15:42:52 -0400

    Triangle Odd numbers Solution

--- 7kyu/SumOfOddNumbers.js
+++ 7kyu/SumOfOddNumbers.js
@@ -1,3 +1,18 @@
 //  https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
 
 
+n = 10
+
+function rowSumOddNumbers(n){
+     let start = n*(n-1) + 1
+     let row = [start]
+     let sum = 0
+     for (i = 0; i < n - 1; i ++) {
+          row.push(row[i] +2)
+     }
+     for (i=0; i< row.length; i++){
+          sum += row[i]
+     }
+     return sum
+
+}
