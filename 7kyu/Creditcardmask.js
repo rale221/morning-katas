c1109aad57356ef8b7b5437f3a5cd75a7b9c2f21
Rastko <rastkodjurica@yahoo.com>
2021-04-08 15:42:25 -0400

    Credit Card Mask

---
+++ 7kyu/Creditcardmask.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5412509bd436bd33920011bc/train/javascript
+
+
+// Examples
+
+// maskify("4556364607935616") == "############5616"
+// maskify(     "64607935616") ==      "#######5616"
+// maskify(               "1") ==                "1"
+// maskify(                "") ==                 ""
+
+// // "What was the name of your first pet?"
+// maskify("Skippy")                                   == "##ippy"
+// maskify("Nananananananananananananananana Batman!") == "####################################man!"
