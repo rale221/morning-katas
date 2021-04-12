d15efed8a6b7d41002bc2f21eef88d9e04736e2c
Rastko <rastkodjurica@yahoo.com>
2021-04-12 01:29:48 -0400

    Shoters Word MAP method

--- 7kyu/ShortestWord.js
+++ 7kyu/ShortestWord.js
@@ -14,10 +14,24 @@ function findShort(s){
      // Kod ove metode, uz pomoc sort-a mozemo da napravimo niz, 
      // koji ce da poredja reci od njamanje ka najvecoj i obnuto
      // i kada znamo niz, onda uz pomoc indexa mozemo da nadjemo i najmanju rec
-     let string = s.split(' ')
-     let word = string.sort((a, b)=> a.length - b.length)
-     return word[0].length
+
+     // let string = s.split(' ')
+     // let word = string.sort((a, b)=> a.length - b.length)
+     // return word[0].length
      
+
+// --------------------------------------------------
+     // MAP method
+     // Kod ove metode, uz pomoc map-a smo prosli kroz array
+     // dobili smo duzinu svake reci, i zatim uz pomoc
+     // Math min ili max dobili najduzu ili u nasem slucaju najkracu rec
+     let string = s.split(' ')
+     let lenghtOfEachWord = string.map(item => item.length)
+     let word = Math.min(...lenghtOfEachWord)
+     return word
+     
+
+
      
      
      
