dea56eab246b87c80ad4ac4d2a0ade210be6ecbf
Rastko <rastkodjurica@yahoo.com>
2021-04-13 14:00:15 -0400

    Persistance

---
+++ 6kyu/PersistentBugger.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
+
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+// which is the number of times you must multiply the digits in num until you reach a single digit.
