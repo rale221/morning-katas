fc34c225f32fbf38d0359c18a30fb13c477140eb
Rastko <rastkodjurica@yahoo.com>
2021-04-16 16:05:32 -0400

    Find the odd int Sort method

--- 6kyu/FindTheOddInt.js
+++ 6kyu/FindTheOddInt.js
@@ -8,10 +8,40 @@
 
 arr = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
 
+// SOLUTION 1 ^XOR 
+
+// function findOdd(arr){
+
+//      let count = arr.reduce((a, b) => a ^ b )
+//      return count
+     
+     
+// }
+
 function findOdd(arr){
 
-     let count = arr.reduce((a, b) => a ^ b )
+     let count = arr.reduce((a, b) => a  )
      return count
      
      
 }
+
+// SOLUTION 2 SORT
+
+function findOdd(arr) {
+     var result, num = 0;
+   
+     arr = arr.sort();
+     for (var i = 0; i < arr.length; i++) {
+       if (arr[i] === arr[i+1]) {
+         num++;
+       } else {
+         num++;
+         if (num % 2 != 0) {
+           result = arr[i];
+           break;
+         }
+       }
+     }
+     return result;
+   }
