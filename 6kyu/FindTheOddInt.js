0562115754041aec89f3deaefca39c160ffd6151
Rastko <rastkodjurica@yahoo.com>
2021-04-16 14:57:17 -0400

    Find the odd integer using ^ (XOR)

--- 6kyu/FindTheOddInt.js
+++ 6kyu/FindTheOddInt.js
@@ -6,4 +6,12 @@
 
 // There will always be only one integer that appears an odd number of times.
 
+arr = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
 
+function findOdd(arr){
+
+     let count = arr.reduce((a, b) => a ^ b )
+     return count
+     
+     
+}
