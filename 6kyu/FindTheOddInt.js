5704253cd2ef8aa601955bd1dc36250e75032a71
Rastko <rastkodjurica@yahoo.com>
2021-04-16 16:49:29 -0400

    Find the odd int Find, Filter

--- 6kyu/FindTheOddInt.js
+++ 6kyu/FindTheOddInt.js
@@ -18,30 +18,51 @@ arr = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
      
 // }
 
-function findOdd(arr){
 
-     let count = arr.reduce((a, b) => a  )
-     return count
-     
-     
-}
 
 // SOLUTION 2 SORT
 
-function findOdd(arr) {
-     var result, num = 0;
+// function findOdd(arr) {
+//      let result, num = 0;
    
-     arr = arr.sort();
-     for (var i = 0; i < arr.length; i++) {
-       if (arr[i] === arr[i+1]) {
-         num++;
-       } else {
-         num++;
-         if (num % 2 != 0) {
-           result = arr[i];
-           break;
-         }
-       }
-     }
-     return result;
+//      arr = arr.sort();
+//      for ( i = 0; i < arr.length; i++) {
+//        if (arr[i] === arr[i+1]) {
+//          num++;
+//        } else {
+//          num++;
+//          if (num % 2 != 0) {
+//            result = arr[i];
+//            break;
+//          }
+//        }
+//      }
+//      return result;
+//    }
+
+
+   // SOLUTION 3
+
+//    function findOdd(arr){
+//         let res = 0
+//         let count = 0
+
+//         for (i=0; i<arr.length; i++){
+//              if (arr[i] === arr[i + 1]){
+//                   count++
+//              }
+//              else {
+//                   count++
+//                   if( count % 2 !== 0){
+//                        result = arr[i]
+//                   }
+//              }
+//         }
+//         return result
+//    }
+
+   // SOLUTION 4 Find
+
+   function findOdd(arr) {
+     return arr.find((item, index) => arr.filter(el => el == item).length % 2)
    }
