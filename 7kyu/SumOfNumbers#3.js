20606fb98212c71ec3fe9b29c617299ec0e417a5
Rastko <rastkodjurica@yahoo.com>
2021-04-18 22:14:51 -0400

    Get Sum Between 2 Integers

---
+++ 7kyu/SumOfNumbers#3.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/55f2b110f61eb01779000053/train/javascript
+
+// Given two integers a and b, which can be positive or negative, 
+// find the sum of all the integers between including them too and return it. 
+// If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples
+
+// GetSum(1, 0) == 1   // 1 + 0 = 1
+// GetSum(1, 2) == 3   // 1 + 2 = 3
+// GetSum(0, 1) == 1   // 0 + 1 = 1
+// GetSum(1, 1) == 1   // 1 Since both are same
+// GetSum(-1, 0) == -1 // -1 + 0 = -1
+// GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+
+function getSum(a,b){
+     
+}
