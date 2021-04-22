3b9d867f35cd1d11dbf0ae3d3bc491178a02a829
Rastko <rastkodjurica@yahoo.com>
2021-04-22 18:28:04 -0400

    Sum of singles Solution 2 4Loop

--- 7kyu/SumOfArraySingles.js
+++ 7kyu/SumOfArraySingles.js
@@ -7,9 +7,29 @@
 // and their sum is 15. Every other number occurs twice.
 
 arr = [4,5,7,5,4,8]
+// SOLUTION 1 4LOOP
 
 function repeats(arr){
 
      return arr.filter(v => arr.indexOf(v) === arr.lastIndexOf(v)).reduce((a,b) => a + b, 0);
-     
+
    };
+
+
+//    // SOLUTION 2 4LOOP
+
+//    function repeats(arr) {
+//      let i;
+//      let sum = 0;
+//      arr.sort((a, b) => {
+//        return a - b;
+//      });
+//      for (i = 0; i < arr.length; i += 2) {
+//        if (arr[i] !== arr[i + 1]) {
+//          sum += arr[i];
+//          i--;
+//        }
+//      }
+   
+//      return sum;
+//    };
