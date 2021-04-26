3dbb8e7773802cad8f5e9335a98319e4eb6b10f2
Rastko <rastkodjurica@yahoo.com>
2021-04-26 18:40:41 -0400

    Return the min Solution 1

--- 7kyu/RemoveTheMinimum.js
+++ 7kyu/RemoveTheMinimum.js
@@ -12,7 +12,9 @@
 // removeSmallest([5,3,2,1,4]) = [5,3,2,4]
 // removeSmallest([2,2,1,2,1]) = [2,2,2,1]
 
+numbers = [1,2,3,4,5]
 
-function removeSmallest(numbers){
-     
-}
+function removeSmallest(numbers) {
+     let indexOfMin = numbers.indexOf(Math.min(...numbers));
+     return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+   }
