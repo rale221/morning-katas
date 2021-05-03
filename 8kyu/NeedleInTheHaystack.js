59d57eb369de8abd9aa31ba6a30e1941ae8e968c
Rastko <rastkodjurica@yahoo.com>
2021-05-02 20:58:56 -0400

    Needle in the haystack

---
+++ 8kyu/NeedleInTheHaystack.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
+
+
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+
+haystack = ['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']
