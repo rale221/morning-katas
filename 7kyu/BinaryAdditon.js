38043f0bd760711e82dc7da884159d59598fedb1
Rastko <rastkodjurica@yahoo.com>
2021-05-05 19:09:08 -0400

    Binary Addition Solution 2

--- 7kyu/BinaryAdditon.js
+++ 7kyu/BinaryAdditon.js
@@ -4,6 +4,22 @@
 // The conversion can be done before, or after the addition.
 // The binary number returned should be a string.
 
+
+//SOLUTION 1
+
 function addBinary(a, b){
      return (a+b).toString(2)
 }
+
+
+//SOLUTION 2
+
+function addBinary(a,b) {
+     var c = a + b;
+     var res = '';
+     while (c >= 1) {
+       var res = c % 2 + res;
+       c = Math.floor(c / 2);
+     }
+     return res;
+   }
