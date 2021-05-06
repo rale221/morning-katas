4e80c7625e472b5c831289b347e4f3a075bf92ef
Rastko <rastkodjurica@yahoo.com>
2021-05-06 17:12:28 -0400

    Clerk Tickets solution's 1&2

--- 6kyu/Clerk.js
+++ 6kyu/Clerk.js
@@ -18,34 +18,34 @@ people = [25,25,50]
 
 //SOLUTION 1
 
-function ticket(peopleInLine){
-     var bills = [0, 0, 0]
-     for (var i = 0; i < peopleInLine.length; i++) {
-       switch (peopleInLine[i]) {
-         case 25:
-           bills[0]++
-           break
+// function ticket(peopleInLine){
+//      var bills = [0, 0, 0]
+//      for (var i = 0; i < peopleInLine.length; i++) {
+//        switch (peopleInLine[i]) {
+//          case 25:
+//            bills[0]++
+//            break
            
-         case 50:
-           bills[0]--
-           bills[1]++
-           break
+//          case 50:
+//            bills[0]--
+//            bills[1]++
+//            break
            
-         case 100:
-           bills[1] ? bills[1]-- : bills[0] -= 2
-           bills[0]--
-           break
-       }
+//          case 100:
+//            bills[1] ? bills[1]-- : bills[0] -= 2
+//            bills[0]--
+//            break
+//        }
        
-       if (bills[0] < 0) {
-         return 'NO'
-       }
-     }
+//        if (bills[0] < 0) {
+//          return 'NO'
+//        }
+//      }
      
-     return 'YES'
+//      return 'YES'
      
 
-}
+// }
 
 
 // SOLUTION 2
