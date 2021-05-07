7b8f63ab31e2c2f531c716a2ea8ebd2501e24dfc
Rastko <rastkodjurica@yahoo.com>
2021-05-07 14:29:42 -0400

    Two to One Solution 1

--- 7kyu/TwoToOne.js
+++ 7kyu/TwoToOne.js
@@ -5,11 +5,17 @@
 
 // Examples:
 
-a = 'xyaabbbccccdefww';
-b = 'xxxxyyyyabklmopq';
+s1 = 'xyaabbbccccdefww';
+s2 = 'xxxxyyyyabklmopq';
 // longest(a, b) -> "abcdefklmopqwxy"
 
 // a = "abcdefghijklmnopqrstuvwxyz"
 // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 
-function longest(s1, s2) {}
+function longest(s1, s2) {
+	let str = s1.concat(s2);
+	let arr = str.split('');
+	let uniq = arr.filter((e, i) => arr.indexOf(e) == i);
+	let res = uniq.sort();
+	return res.join('');
+}
