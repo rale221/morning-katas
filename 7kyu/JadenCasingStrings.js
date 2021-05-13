95d6b5e34823cde8b5225df4d446f7cdb32469e1
Rastko <rastkodjurica@yahoo.com>
2021-05-13 19:53:59 -0400

    Jaden Chasing Strings Solution 2

--- 7kyu/JadenCasingStrings.js
+++ 7kyu/JadenCasingStrings.js
@@ -8,8 +8,16 @@
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
+// SOLUTION 1
+
 String.prototype.toJadenCase = function () { 
      return this.split(" ").map(function(word){
        return word.charAt(0).toUpperCase() + word.slice(1);
      }).join(" ");
    }
+
+// SOLUTION 2
+
+String.prototype.toJadenCase = function() {
+     return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
+   };
