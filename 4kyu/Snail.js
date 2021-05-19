db436aefc80ecc95b8908b5fef0720ebbdc1c67c
Rastko <rastkodjurica@yahoo.com>
2021-05-18 22:35:12 -0400

    Snail Solution 1

--- 4kyu/Snail.js
+++ 4kyu/Snail.js
@@ -18,3 +18,22 @@
 // NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
 
 // NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+
+// SOLUTION 1
+
+snail = function(array) {
+     var result;
+     while (array.length) {
+       // Steal the first row.
+       result = (result ? result.concat(array.shift()) : array.shift());
+       // Steal the right items.
+       for (var i = 0; i < array.length; i++)
+         result.push(array[i].pop());
+       // Steal the bottom row.
+       result = result.concat((array.pop() || []).reverse());
+       // Steal the left items.
+       for (var i = array.length - 1; i >= 0; i--)
+         result.push(array[i].shift());
+     }
+     return result;
+   }
