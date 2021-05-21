e284beab96a7638b06788737a6e02d7cd744a5fc
Rastko <rastkodjurica@yahoo.com>
2021-05-21 19:14:35 -0400

    Consecutive Strings Solution 1

--- 6kyu/ConsecutiveStrings.js
+++ 6kyu/ConsecutiveStrings.js
@@ -25,3 +25,19 @@
 // Note
 
 // consecutive strings : follow one after another without an interruption
+
+
+// SOLUTION 1
+
+function longestConsec(strarr, k) {
+     var longest = "";
+     for(var i=0;k>0 && i<=strarr.length-k;i++){
+       var tempArray = strarr.slice(i,i+k);
+       var tempStr = tempArray.join("");
+       if(tempStr.length > longest.length){
+         longest = tempStr;
+       }
+     }
+     return longest;
+ }
+ 
