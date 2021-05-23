e6e72ee436cdf657594234b23d961a8e893f99ca
Rastko <rastkodjurica@yahoo.com>
2021-05-23 19:23:53 -0400

    Sort the odd solution 1

--- 6kyu/SortTheOdd.js
+++ 6kyu/SortTheOdd.js
@@ -10,3 +10,10 @@
 // [7, 1]  =>  [1, 7]
 // [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+function sortArray(array) {
+   let odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+     return array.map((x) => x % 2 ? odd.shift() : x);
+   }
