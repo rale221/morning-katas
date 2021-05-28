fcd4944dea7bc43d36abc0a3a46fb2922937868c
Rastko <rastkodjurica@yahoo.com>
2021-05-28 17:13:49 -0400

    The total points solution 1

--- 8kyu/TotalAmountOfPoints.js
+++ 8kyu/TotalAmountOfPoints.js
@@ -16,6 +16,16 @@
 // 0 <= x <= 4
 // 0 <= y <= 4
 
-function points(games){
-     
-}
+games = ['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3']
+
+function points(games) {
+     let total = 0;
+     games.map(game => {
+       if (game[0] === game[2]) {
+         total += 1;
+       } else if (game[0] > game[2]) {
+         total += 3;
+       }
+     });
+     return total;
+   }
