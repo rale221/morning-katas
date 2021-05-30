9d8dfaeedce78caafa97d1e7b415e67e04ab4502
Rastko <rastkodjurica@yahoo.com>
2021-05-29 22:12:59 -0400

    Solution 2

--- 4kyu/NextBiggerNumSameDigits.js
+++ 4kyu/NextBiggerNumSameDigits.js
@@ -40,3 +40,21 @@ function nextBigger(n){
      console.log("->" +num);
      return num;
    }
+
+   // Sulution 2
+
+   const sortedDigits = n => { let arr = n.toString().split(''); arr.sort((a, b) => b - a); return arr; };
+
+function nextBigger(n){
+  
+  let arr = sortedDigits(n);
+  let max = parseInt(arr.join(''), 10);
+  
+  for(var i = n + 1; i <= max; i++){
+    if(sortedDigits(i).every((x, j) => x === arr[j])){
+      return i;
+    }
+  }
+  
+  return -1;
+}
