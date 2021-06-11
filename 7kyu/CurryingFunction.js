bb0bb06fc83f3785ad7bc240ce7729500d1a4837
Rastko <rastkodjurica@yahoo.com>
2021-06-11 18:50:38 -0400

    Currying function solution 1

---
+++ 7kyu/CurryingFunction.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
+
+// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+// Example:
+
+// multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+
+list = [2, 3, 4]
+n = [2]
+
+function multiplyAll(list){
+
+     return function end(n){
+         let a =  list.map((e) => e*n)
+         return a
+
+}}
