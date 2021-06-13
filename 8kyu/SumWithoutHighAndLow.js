95bbe43b87a41dca6b72e2932277d2019d5a80f3
Rastko <rastkodjurica@yahoo.com>
2021-06-13 14:48:11 -0400

    sum without high and low number solution 1

--- 8kyu/SumWithoutHighAndLow.js
+++ 8kyu/SumWithoutHighAndLow.js
@@ -9,3 +9,15 @@
 // { 1, 1, 11, 2, 3 } => 6
 // If array is empty, null or None, or if only 1 Element exists, return 0.
 // Note:In C++ instead null an empty vector is used. In C there is no null. ;-)
+
+array = [6, 2, 1, 8, 10];
+
+function sumArray(array) {
+	let sortArray = array.sort((a, b) => a - b);
+	let sum = 0;
+	for (i = 1; i < sortArray.length - 1; i++) {
+		sum += sortArray[i];
+	}
+
+	return sum;
+}
