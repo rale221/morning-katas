5d0cee6c68fd7b22bf62529eeaeb7a182e75f9f3
Rastko <rastkodjurica@yahoo.com>
2021-06-20 13:27:38 -0400

    Spead Control solution 1

--- 7kyu/SpeedControl.js
+++ 7kyu/SpeedControl.js
@@ -24,5 +24,15 @@
 // (3600 * delta_distance) / s.
 
 function gps(s, x){
+     if (x.length<=1) {
+          return 0;
+        }
+        
+        let output = [];
+        for (let i = 0; i < x.length-1; i++) { 
+          output.push((x[i+1]-x[i])*3600/s);
+        }
+        
+        return Math.max(...output);
      
 }
