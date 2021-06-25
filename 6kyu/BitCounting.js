be10d95cb67955792e0b23ecc25275e02e4e0e13
Rastko <rastkodjurica@yahoo.com>
2021-06-25 18:56:45 -0400

    bit counting solution 2 and 3

--- 6kyu/BitCounting.js
+++ 6kyu/BitCounting.js
@@ -7,8 +7,22 @@
 // Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 n = 1234
 
+// Solution 1
+
+// function countBits(n){
+// let base = n.toString(2).split('')
+// let result = base.reduce((a, b) => a + Number(b), 0)
+// return result
+// }
+
+
+// Solution 2 and 3
+
+// function countBits(n){
+//      return n.toString(2).replace(/0/g,'').length
+//    };
+
+
 function countBits(n){
-let base = n.toString(2).split('')
-let result = base.reduce((a, b) => a + Number(b), 0)
-return result
+     return n.toString(2).split('0').join('').length
 }
