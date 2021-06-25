d3361d82954b812aa552118ef543161eac3b314c
Rastko <rastkodjurica@yahoo.com>
2021-06-25 18:52:41 -0400

    But counting solution 1

--- 6kyu/BitCounting.js
+++ 6kyu/BitCounting.js
@@ -5,7 +5,10 @@
 // You can guarantee that input is non-negative.
 
 // Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+n = 1234
 
 function countBits(n){
-
+let base = n.toString(2).split('')
+let result = base.reduce((a, b) => a + Number(b), 0)
+return result
 }
