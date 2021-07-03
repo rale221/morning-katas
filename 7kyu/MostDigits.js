3104dc9b81bdf3f1a4d2d91e0d2e6ead045b7e03
Rastko <rastkodjurica@yahoo.com>
2021-07-03 13:34:51 -0400

    Most digits in array

---
+++ 7kyu/MostDigits.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/58daa7617332e59593000006/train/javascript
+
+// Find the number with the most digits.
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+array = [1, 10, 7, 16, 15, 28, 20]
+
+function findLongest(array){
+
+}
+
