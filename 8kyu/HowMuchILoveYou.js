ce47943b88164ca5b2662088e6d7391fd682bb33
Rastko <rastkodjurica@yahoo.com>
2021-07-08 17:32:33 -0400

    how much solution 1 using array of object

--- 8kyu/HowMuchILoveYou.js
+++ 8kyu/HowMuchILoveYou.js
@@ -13,7 +13,15 @@
 // Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
 
 nbPetals = 7
-function howMuchIloveYou(nbPetals){
-
-}
+function howMuchILoveYou(nbPetals) {
+     let phrase = {    
+       0: "not at all",
+       1: "I love you",
+       2: "a little",
+       3: "a lot",
+       4: "passionately",
+       5: "madly"
+     }
+     return phrase[nbPetals%6]
+   }
 
