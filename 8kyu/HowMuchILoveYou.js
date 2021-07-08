8f3373c639ea7095ec5a6a92d4e2cdc63c5b8bc7
Rastko <rastkodjurica@yahoo.com>
2021-07-08 17:34:52 -0400

    how much solution 2

--- 8kyu/HowMuchILoveYou.js
+++ 8kyu/HowMuchILoveYou.js
@@ -13,15 +13,32 @@
 // Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
 
 nbPetals = 7
+let phrase = {    
+     0: "not at all",
+     1: "I love you",
+     2: "a little",
+     3: "a lot",
+     4: "passionately",
+     5: "madly"
+   }
 function howMuchILoveYou(nbPetals) {
-     let phrase = {    
-       0: "not at all",
-       1: "I love you",
-       2: "a little",
-       3: "a lot",
-       4: "passionately",
-       5: "madly"
-     }
      return phrase[nbPetals%6]
    }
+
+
+   // solution 2
+
+  let phrases = [ 'I love you',
+     'a little',
+     'a lot',
+     'passionately',
+     'madly',
+     'not at all',
+ ]
+ 
+ function howMuchILoveYou(n) {
+      return phrases[(n - 1) % phrases.length] 
+ }
+
+
 
