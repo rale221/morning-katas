04706b5139bb49354bfbb0afb41fca6c266566ee
Rastko <rastkodjurica@yahoo.com>
2021-07-09 20:41:01 -0400

    Reversed Words

---
+++ 8kyu/ReversedWords.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Example:
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+str = "The greatest victory is that which requires no battle"
+
+function reverseWords(str){
+
+}
