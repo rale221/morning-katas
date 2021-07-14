733f1b7f39a547d07692213f2fb7d1af1131374f
Rastko <rastkodjurica@yahoo.com>
2021-07-13 21:09:14 -0400

    Scramblies solution 1

--- 5kyu/Scramblies.js
+++ 5kyu/Scramblies.js
@@ -11,3 +11,10 @@
 // scramble('cedewaraaossoqqyt', 'codewars') ==> True
 // scramble('katas', 'steak') ==> False
 
+
+// SOLUTUION 1
+
+function scramble(str1, str2) {
+     let occurences = str1.split("").reduce((arr, cur) => { arr[cur] ? arr[cur]++ : arr[cur] = 1; return arr; }, {});
+     return str2.split("").every((character) => --occurences[character] >= 0);
+   }
