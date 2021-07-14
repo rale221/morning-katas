3c0ec7802b378ea2cdac1284b9b520930e36b142
Rastko <rastkodjurica@yahoo.com>
2021-07-13 21:10:17 -0400

    Scramblies solution 2

--- 5kyu/Scramblies.js
+++ 5kyu/Scramblies.js
@@ -18,3 +18,25 @@ function scramble(str1, str2) {
      let occurences = str1.split("").reduce((arr, cur) => { arr[cur] ? arr[cur]++ : arr[cur] = 1; return arr; }, {});
      return str2.split("").every((character) => --occurences[character] >= 0);
    }
+
+// SOLUTION 2
+
+function scramble(strToBeChecked, strToCheckFor) {
+     let numLetters = {}
+   
+     for (const letter of strToCheckFor) {
+       if (numLetters[letter]) numLetters[letter]++
+       else numLetters[letter] = 1
+     }
+   
+     for (const letter of strToBeChecked) {
+       if (numLetters[letter] && numLetters[letter] !== 0) numLetters[letter]--
+     }
+   
+     for (const key in numLetters) {
+       if (numLetters[key] !== 0) return false
+     }
+   
+     // Only reaches this far if all good
+     return true
+   }
