77259c3a81a15d3ede72fb90294aa410ba233dd3
Rastko <rastkodjurica@yahoo.com>
2021-07-16 18:13:32 -0400

    Squared strings sol 1

--- 5kyu/SquaredStrings.js
+++ 5kyu/SquaredStrings.js
@@ -42,3 +42,25 @@
 // Fortran: Your returned string for both functions are not permitted to contain redundant leading/trailing whitespace. In return, you may safely assume that all input strings passed into your function(s) will not contain redundant leading/trailing whitespace, i.e. you do not and should not trim the input string before operating on it
 
 // Don't use this coding to keep your secrets:-)
+
+function code(s) {
+	let str = s.replace(/\n/g, ''),
+		len = str.length,
+		res = '';
+
+	let n = Math.ceil(Math.sqrt(len)),
+		m = n * (n - 1);
+
+	for (var i = 0; i < n; i++) {
+		res += '\n';
+
+		for (var j = 0; j < n; j++) res += str[m + i - n * j] || '\v';
+	}
+
+	return res.slice(1);
+}
+
+function decode(s) {
+	let res = code(code(code(s)));
+	return res.replace(/(\n|\v)/g, '');
+}
