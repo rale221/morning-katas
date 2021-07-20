aec7108920bec5cfa94206c58636fe3d0cdd6895
Rastko <rastkodjurica@yahoo.com>
2021-07-20 18:00:52 -0400

    Categorize members solution 2

--- 7kyu/CategorizeNewMember.js
+++ 7kyu/CategorizeNewMember.js
@@ -35,3 +35,11 @@ function openOrSenior(data){
      return b
 
 }
+
+
+// Solution 2
+// function openOrSenior(data){
+//      return data.map(function(d){
+//        return d[0] >= 55 && d[1] > 7 ? 'Senior' : 'Open';
+//      });
+//    }
