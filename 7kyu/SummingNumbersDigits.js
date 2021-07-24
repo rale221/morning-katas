244cfa0c1454b29b06fbfdeb60a223aa7baa1b04
Rastko <rastkodjurica@yahoo.com>
2021-07-24 11:35:57 -0400

    Summing Numbers Digits solution 2

--- 7kyu/SummingNumbersDigits.js
+++ 7kyu/SummingNumbersDigits.js
@@ -14,3 +14,10 @@ function sumDigits(number){
      const res = arr.reduce((a, b) => a+b)
      return res
 }
+
+
+// Solution 2
+
+function sumDigits(number) {
+     return Math.abs(number).toString().split('').reduce(function(a,b){return +a + +b}, 0);
+   }
