3582d7d7002383ab7cca373c18725206fd33e1a2
Rastko <rastkodjurica@yahoo.com>
2021-07-31 13:13:53 -0400

    fwill you make it solution

--- 8kyu/WillYouMakeIt.js
+++ 8kyu/WillYouMakeIt.js
@@ -5,3 +5,15 @@
 // You know that on average, your car runs on about 25 miles per gallon. 
 // There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog) if it is possible and false (0 in Prolog) if not. 
 // The input values are always positive.
+distanceToPump = 100
+mpg = 50
+fuelLeft = 1
+
+function zeroFuel(distanceToPump, mpg, fuelLeft){
+     if ( (fuelLeft * mpg) >= distanceToPump ){
+          return true
+     } else {
+          return false
+     }
+     
+}
