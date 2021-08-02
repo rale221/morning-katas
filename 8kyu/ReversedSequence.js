1820784dc7516a88a68a91ca4aeb1db2590b0c48
Rastko <rastkodjurica@yahoo.com>
2021-08-02 17:57:47 -0400

    Reversed Sequence

---
+++ 8kyu/ReversedSequence.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/javascript
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+function reverseSeq(n){
+
+}
