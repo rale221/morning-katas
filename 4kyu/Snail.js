3354e99dc36ebe2016b2fb0cfa407a20ba706433
Rastko <rastkodjurica@yahoo.com>
2021-08-04 13:07:51 -0400

    snail sol 2

--- 4kyu/Snail.js
+++ 4kyu/Snail.js
@@ -21,31 +21,31 @@
 
 // SOLUTION 1
 
-snail = function(array) {
-     var result;
-     while (array.length) {
-       // Steal the first row.
-       result = (result ? result.concat(array.shift()) : array.shift());
-       // Steal the right items.
-       for (var i = 0; i < array.length; i++)
-         result.push(array[i].pop());
-       // Steal the bottom row.
-       result = result.concat((array.pop() || []).reverse());
-       // Steal the left items.
-       for (var i = array.length - 1; i >= 0; i--)
-         result.push(array[i].shift());
-     }
-     return result;
-   }
+// snail = function(array) {
+//      var result;
+//      while (array.length) {
+//        // Steal the first row.
+//        result = (result ? result.concat(array.shift()) : array.shift());
+//        // Steal the right items.
+//        for (var i = 0; i < array.length; i++)
+//          result.push(array[i].pop());
+//        // Steal the bottom row.
+//        result = result.concat((array.pop() || []).reverse());
+//        // Steal the left items.
+//        for (var i = array.length - 1; i >= 0; i--)
+//          result.push(array[i].shift());
+//      }
+//      return result;
+//    }
 
-   // SOLUTION 2
+//    // SOLUTION 2
 
-   function snail(array) {
-     var vector = [];
-     while (array.length) {
-       vector.push(...array.shift());
-       array.map(row => vector.push(row.pop()));
-       array.reverse().map(row => row.reverse());
-     }
-     return vector;
-   }
+//    function snail(array) {
+//      var vector = [];
+//      while (array.length) {
+//        vector.push(...array.shift());
+//        array.map(row => vector.push(row.pop()));
+//        array.reverse().map(row => row.reverse());
+//      }
+//      return vector;
+//    }
