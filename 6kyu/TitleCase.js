cd15842497045d1008689e56adebcb5fde83476d
Rastko <rastkodjurica@yahoo.com>
2021-08-09 17:37:29 -0400

    title case example 3 using string ptototype

--- 6kyu/TitleCase.js
+++ 6kyu/TitleCase.js
@@ -55,3 +55,18 @@ function titleCase(title, minorWords) {
      return cap(result);
    
    }
+
+   // Solution 3
+
+   String.prototype.capitalize = function() {
+     return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+ }
+ 
+ function titleCase(title, minorWords) {  
+   var titleAr = title.toLowerCase().split(' '),
+       minorWordsAr = minorWords ? minorWords.toLowerCase().split(' ') : [];
+     
+   return titleAr.map(function(e, i){return minorWordsAr.indexOf(e) === -1 || i === 0 ? e.capitalize() : e })
+                 .join(' ');
+ }
+ 
