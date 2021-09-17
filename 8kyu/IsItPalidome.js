79ccf6d1188be93625dfe9f616681066be62ac95
Rastko <rastkodjurica@yahoo.com>
2021-09-17 18:13:50 -0400

    palidrome solution 1

--- 8kyu/IsItPalidome.js
+++ 8kyu/IsItPalidome.js
@@ -1,7 +1,6 @@
 // https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
 
 // Write a function that checks if a given string (case insensitive) is a palindrome.
+x = 'Palidrome'
 
-function isPalindrome(x) {
-   
-   }
+const isPalindrome = x => x.toLowerCase() === x.toLowerCase().split('').reverse().join('');
