1ee897e471170ffc5ae2fcdf90456a1897cc18d6
Rastko <rastkodjurica@yahoo.com>
2021-09-22 20:20:54 -0400

    twice as old

---
+++ 8kyu/TwiceAsOld.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+     // your code here
+   }
