53fc0b15e7d0d29e157defa1bbafa0708003b341
Rastko <rastkodjurica@yahoo.com>
2021-09-27 19:25:33 -0400

    swapin vowels

---
+++ 7kyu/SwapVowelsCase.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5803c0c6ab6c20a06f000026/train/javascript
+
+// Challenge:
+
+// Given a string, return a copy of the string with the vowels' case swapped.
+
+// For this kata, assume that vowels are in the set "aeouiAEOUI".
+
+// Example: Given a string "C is alive!", your function should return "C Is AlIvE!"
+
+// Addendum: Your solution is only required to work for the ASCII character set.
+
+// Please make sure you only swap cases for the vowels.
+
+
+function swapVowelCase(str) {
+   
+     // your code here
+   }
